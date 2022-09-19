8cd9ad5e8d940ddae49b0b01f6f8d06ffa568a49
Ryusaki <kempcompany05@gmail.com>
2022-09-19 22:51:37 +0200

    Initial Commit

---
+++ Inus Stream/Inus_StreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  Inus_StreamApp.swift
+//  Inus Stream
+//
+//  Created by L Lawliet on 19.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Inus_StreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
